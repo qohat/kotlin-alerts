3ff19e4bcfab3f940e202ee66e9ac12eaad89262
Qohat <qohatpp@gmail.com>
2022-07-22 11:25:14 -0500

    fixing persistence

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,28 +53,17 @@ tasks {
 }
 
 dependencies {
-    implementation(libs.arrow.core)
-    implementation(libs.arrow.fx)
+    implementation(libs.bundles.arrow)
+    implementation(libs.bundles.ktor.server)
     implementation(libs.ktor.serialization)
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.cors)
-    implementation(libs.ktor.server.defaultheaders)
-    implementation(libs.ktor.server.content.negotiation)
-    implementation(libs.ktor.server.netty)
-    implementation(libs.ktor.server.auth)
     implementation(libs.logback.classic)
     implementation(libs.sqldelight.jdbc)
     implementation(libs.hikari)
     implementation(libs.postgresql)
     implementation(libs.kotlin.kafka)
 
-    testImplementation(libs.ktor.client.content.negotiation)
-    testImplementation(libs.ktor.client.serialization)
+    testImplementation(libs.bundles.ktor.client)
     testImplementation(libs.testcontainers.postgresql)
     testImplementation(libs.ktor.server.tests)
-    testImplementation(libs.kotest.arrow)
-    testImplementation(libs.kotest.runnerJUnit5)
-    testImplementation(libs.kotest.frameworkEngine)
-    testImplementation(libs.kotest.assertionsCore)
-    testImplementation(libs.kotest.property)
+    testImplementation(libs.bundles.kotest)
 }
